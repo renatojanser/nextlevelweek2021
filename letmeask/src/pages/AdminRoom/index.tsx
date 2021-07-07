94b4d693d3a03feede8c15b2ec0c5d3b61169363
Renato Janser <renato.janser@gmail.com>
2021-07-07 12:33:11 -0300

    Excluir pergunta e inativar a sala

--- letmeask/src/pages/AdminRoom/index.tsx
+++ letmeask/src/pages/AdminRoom/index.tsx
@@ -1,30 +1,44 @@
-// import { FormEvent, useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
-// import { useAuth } from '../../hooks/useAuth';
 import { useRoom } from '../../hooks/useRoom';
 
-// import { database } from '../../services/firebase';
+import { database } from '../../services/firebase';
 
 import { RoomCode } from '../../components/RoomCode';
 import { Question } from '../../components/Question';
 
 import logoImg from '../../assets/images/logo.svg';
+import deleteImg from '../../assets/images/delete.svg';
 
 import { Button } from '../../components/Button';
 
 import './styles.scss';
+
 
 type RoomParams = {
     id: string;
 }
 
 export function AdminRoom() {
-    // const {user} = useAuth();
+    const history = useHistory();
     const params = useParams<RoomParams>();
     const roomId = params.id;
 
     const { title, questions } = useRoom(roomId);
+
+    async function handleEndRoom() {
+        await database.ref(`rooms/${roomId}`).update({
+            endedAt: new Date() 
+        });
+
+        history.push('/');
+    }
+
+    async function handleDeleteQuestion(questionId: string) {
+        if(window.confirm('Tem certeza que deseja excluir essa pergunta?')) {
+            await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+        }
+    }
 
     return (
         <div id="page-room">
@@ -33,7 +47,7 @@ export function AdminRoom() {
                     <img src={logoImg} alt="Letmeask" />
                     <div>
                         <RoomCode code={roomId} />
-                        <Button isOutlined >Encerrar sala</Button>
+                        <Button isOutlined onClick={handleEndRoom} >Encerrar sala</Button>
                     </div>
                 </div>
             </header>
@@ -51,7 +65,14 @@ export function AdminRoom() {
                                 key={question.id}
                                 content={question.content}
                                 author={question.author}
-                            />
+                            >
+                                <button
+                                    type="button"
+                                    onClick={() => handleDeleteQuestion(question.id)}
+                                >
+                                    <img src={deleteImg} alt="Excluir pergunta" />
+                                </button>
+                            </Question>
                         );
                     })}
                 </div>
